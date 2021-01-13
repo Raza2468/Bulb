3e0e94bfbe98064f7cdfefe0447a8159de6548be
raza <raza2468@gmail.com>
2021-01-13 20:27:11 +0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,10 @@
+function on() {
+   var on = document.getElementById("bulf").src = "https://eportfoliojoebarker.files.wordpress.com/2015/05/light_bulb_on.png";
+ 
+}
+function off() {
+    var off = document.getElementById("bulf").src ="https://eportfoliojoebarker.files.wordpress.com/2015/05/light_bulb_off.png"
+
+}
+
+    
